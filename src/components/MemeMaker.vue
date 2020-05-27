dde910ca201e09998e95d6ed26c99a771d03d08a
Derek Johnston <Derek@dj-design.ca>
2020-05-27 14:57:20 -0400

    meme generater working v1

--- src/components/MemeMaker.vue
+++ src/components/MemeMaker.vue
@@ -3,17 +3,17 @@
     .row(v-if="!selectedImage")
       .col
         h5 Select an Image
-    .row.bg-success.p-2.rounded(v-else)
+    .row.border.p-2.rounded(v-else)
       .col-12
         .row
           .col
-            div(contenteditable="false")
-              svg(height="400"  length="auto")
+            .row
+              svg(height="400")
                 image(:href="selectedImage.url" height="400")            
                 text(:x="string.x + 'em'" :y="string.y + 'em'"
                   v-for="(string,index) in strings"
                   :fill="strings[index].color"
-                  font-size="1em") {{string.text}}
+                  :font-size="string.size + 'em'") {{string.text}}
           .col
             .row
               b-form-group(
@@ -51,23 +51,28 @@
                         type="number"
                         required
                         placeholder="Enter text")
+                .row.py-2
+                  .col-4 Size
+                  .col-8
+                    b-form-input(
+                      id="input-2"
+                      v-model="strings[index].size"
+                      type="number"
+                      required
+                      placeholder="Enter text")
               .col-4
                 b-button(variant="info" @click="toggleShade(index)") Toggle Shade
             .row.m-1
               .col
                 b-button(variant="success" @click="addString") Add Text
-        .row.p-2(v-if="image")
-          .col-6
-            | IMAGE
-            img(:src="image") 
-          .col-6
-            | CANVAS
-            canvas
         .row.p-2
           .col-12
-            b-button(variant="success" @click="dlCanvas") Download
+            b-button(variant="info" @click="dlCanvas") Generate
+        .row.p-2(v-if="image")
+          .col-12
+            b-button(variant="success" :href="image" download="meme") Download
     .row
-      .col-3-md.p-1(v-for="(image,index) in images" :key="image.id" @click="selectImage(index)")
+      .col-sm-6.col-md-4.col-lg-3.p-1(v-for="(image,index) in images" :key="image.id" @click="selectImage(index)")
         MemeImages(:image='image')
         
 </template>
@@ -84,7 +89,7 @@ export default {
       image: null,
       images: [],
       selectedImage: null,
-      strings: [{ text: "Hello World", color: "white", x: 1, y: 1 }]
+      strings: [{ text: "Hello World", color: "white", x: 1, y: 1 , size:1}]
     };
   },
   mounted() {
@@ -98,11 +103,10 @@ export default {
   },
   methods: {
     selectImage(index) {
-      console.log(this.images[index]);
       this.selectedImage = this.images[index];
     },
     addString() {
-      this.strings.push({ text: "", color: "white", x: 1, y: 1 });
+      this.strings.push({ text: "", color: "white", x: 1, y: 1 , size:1});
     },
     toggleShade(index) {
       this.strings[index].color === "white"
@@ -111,8 +115,6 @@ export default {
     },
     dlCanvas() {
       var svg = document.querySelector("svg");
-      var img = document.querySelector("img");
-      var canvas = document.querySelector("canvas");
 
       // get svg data
       var xml = new XMLSerializer().serializeToString(svg);
@@ -125,20 +127,7 @@ export default {
       var image64 = b64Start + svg64;
 
       // set it as the source of the img element
-      setTimeout(() => this.image = image64, 2000);
-      
-      console.log("image64", image64)
-
-      // draw the image onto the canvas
-      canvas.getContext("2d").drawImage(img, 0, 0);
-
-      var dt = canvas.toDataURL("image/png"); // << this fails in IE/Edge...
-      dt = dt.replace(/^data:image\/[^;]*/, "data:application/octet-stream");
-      dt = dt.replace(
-        /^data:application\/octet-stream/,
-        "data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=Canvas.png"
-      );
-      this.href = dt;
+      this.image = image64;
     }
   }
 };
